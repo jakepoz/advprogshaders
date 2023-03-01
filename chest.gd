ee9b3be2e6ed11b52662d1c322cba5a8969d7385
Jake <jakepoz@gmail.com>
2023-03-01 14:45:05 -0800

    Adding a treasure chest

---
+++ chest.gd
@@ -0,0 +1,13 @@
+extends StaticBody2D
+
+signal opened
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		emit_signal("opened")
